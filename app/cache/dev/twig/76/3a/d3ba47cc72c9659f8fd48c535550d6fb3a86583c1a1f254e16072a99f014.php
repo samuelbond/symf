9951363bb2f0f1c0f125389c83af0b2a5fc8bb50
root <samuelizuchi@gmail.com>
2015-01-13 14:42:45 +0000

    first commit

---
+++ app/cache/dev/twig/76/3a/d3ba47cc72c9659f8fd48c535550d6fb3a86583c1a1f254e16072a99f014.php
@@ -0,0 +1,57 @@
+<?php
+
+/* default/index.html.twig */
+class __TwigTemplate_763ad3ba47cc72c9659f8fd48c535550d6fb3a86583c1a1f254e16072a99f014 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 1
+        try {
+            $this->parent = $this->env->loadTemplate("base.html.twig");
+        } catch (Twig_Error_Loader $e) {
+            $e->setTemplateFile($this->getTemplateName());
+            $e->setTemplateLine(1);
+
+            throw $e;
+        }
+
+        $this->blocks = array(
+            'body' => array($this, 'block_body'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_body($context, array $blocks = array())
+    {
+        // line 4
+        echo "    Homepage.
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "default/index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  39 => 4,  36 => 3,  11 => 1,);
+    }
+}
